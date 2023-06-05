fec798dd66012bd31e7440ae77a2182c536c1618
Michael Pierce <80133921+mpierce1988@users.noreply.github.com>
2023-06-05 20:57:01 +0000

    Begin experimenting with turbo frame tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+before_action -> { sleep 2}
+
+
+def page1; end
+
+def page2; end
+
+def page3
+    redirect_to page2_path status: :see_other
+end
+
+def page4; end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root "hello#index"
+
+  get 'page1', to: 'application#page1'
+  get 'page2', to: 'application#page2'
+  post 'page3', to: 'application#page3'
+  get 'page4', to: 'application#page4'
 end
